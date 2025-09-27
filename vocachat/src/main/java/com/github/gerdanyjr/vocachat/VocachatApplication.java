4cd37e244999d54d131ea55dba391f3da73dbb3d
GerdanyJr <juniorgerdany@gmail.com>
2025-09-27 15:44:22 -0300

    feat: first commit

---
+++ vocachat/src/main/java/com/github/gerdanyjr/vocachat/VocachatApplication.java
@@ -0,0 +1,13 @@
+package com.github.gerdanyjr.vocachat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocachatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocachatApplication.class, args);
+	}
+
+}
---
+++ vocachat/src/test/java/com/github/gerdanyjr/vocachat/VocachatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gerdanyjr.vocachat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocachatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
